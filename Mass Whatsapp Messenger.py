c07e1db3d9544efa87f7c8b864f0f92445a9b056
siratul <asatulsingh20@gmail.com>
2020-07-18 16:06:30 +0530

    Update to increase performance

--- Mass Whatsapp Messenger.py
+++ Mass Whatsapp Messenger.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import time
 
 msg_file = open("Message.txt")
@@ -17,24 +19,23 @@ while(contact_file is None):
 
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com/")
-time.sleep(10) #Wait period for you to log-in
-serviceurl = "https://wa.me"
+time.sleep(10)  # Wait period for you to log-in
+serviceurl = "https://web.whatsapp.com/"
 params = dict()
 params['text'] = msg
 for contact in contact_file:
     contact = contact.rstrip()
-    url = serviceurl + '/' + '91' + contact + '/?' + urllib.parse.urlencode(params)
+    url = serviceurl + 'send?phone=91' + contact + '&' + \
+        urllib.parse.urlencode(params) + "&source&data&app_absent"
     print(contact)
-    try :
+    try:
         if (contact != contact_file):
             driver.execute_script("window.onbeforeunload = function() {};")
         driver.get(url)
         time.sleep(3)
-        driver.find_element_by_xpath("//*[@id='action-button']").click()
-        time.sleep(1)
-        driver.find_element_by_xpath("//*[@id='fallback_block']/div/div/a").click()
-        time.sleep(5)
-        driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
+
+        driver.find_element_by_xpath(
+            "//*[@id='main']/footer/div[1]/div[3]/button").click()
         print("Done!")
     except:
         print("Contact not on whatsapp")
